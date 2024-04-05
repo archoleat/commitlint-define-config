793e71402d01862b3b217f7d8cba16cf894cfe1b
Almanov Nikita <131481562+nikkeyl@users.noreply.github.com>
2024-04-05 14:55:32 +0300

    refactor(architecture): rename `define-config` to `index`

--- specs/index.spec-d.ts
+++ specs/index.spec-d.ts
@@ -2,14 +2,14 @@ import { describe, expectTypeOf, test } from 'vitest';
 
 import type { UserConfig } from '@commitlint/types';
 
-import { defineConfig } from '../define-config.ts';
+import { defineConfig } from '../src/index.ts';
 
-describe('Define Config', () => {
+describe('Define Commitlint Config', () => {
   test('define empty config', () => {
     expectTypeOf(defineConfig({})).toEqualTypeOf<UserConfig>();
   });
 
-  test('define Commitlint config', () => {
+  test('define config', () => {
     expectTypeOf(
       defineConfig({
         defaultIgnores: true,
