8ae8f72435b7bc2c8f685a546f26663070ddbcee
Almanov Nikita <131481562+nikkeyl@users.noreply.github.com>
2024-04-08 14:13:28 +0300

    spec: replace `test` with `spec`

--- specs/index.spec-d.ts
+++ specs/index.spec-d.ts
@@ -1,15 +1,15 @@
-import { describe, expectTypeOf, test } from 'vitest';
+import { describe, expectTypeOf, test as spec } from 'vitest';
 
 import type { UserConfig } from '@commitlint/types';
 
 import { defineConfig } from '../src/index.ts';
 
 describe('Define Commitlint Config', () => {
-  test('define empty config', () => {
+  spec('define empty config', () => {
     expectTypeOf(defineConfig({})).toEqualTypeOf<UserConfig>();
   });
 
-  test('define config', () => {
+  spec('define config', () => {
     expectTypeOf(
       defineConfig({
         defaultIgnores: true,
