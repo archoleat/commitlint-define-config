c9b26de3f03e40f47e11269a2e68aa0a1b10074a
Almanov Nikita <131481562+nikkeyl@users.noreply.github.com>
2024-09-22 23:57:09 +0300

    refactor: specs

--- specs/define-config.spec.ts
+++ specs/define-config.spec.ts
@@ -2,24 +2,204 @@ import { describe, expectTypeOf, test as spec } from 'vitest';
 
 import type { UserConfig } from '@commitlint/types';
 
-import { defineConfig } from '#app';
+import { defineConfig } from '#index';
 
-describe('Commitlint Config', () => {
-  spec('empty config', async () => {
+describe('Commitlint Config', async () => {
+  spec('should return empty config', async () => {
     expectTypeOf(defineConfig({})).toEqualTypeOf<UserConfig>();
   });
 
-  spec('config', async () => {
+  spec('should return config', async () => {
     expectTypeOf(
       defineConfig({
         defaultIgnores: true,
-        extends: [],
-        formatter: 'string',
+        extends: [''],
+        formatter: '',
         helpUrl: '',
-        ignores: [],
-        parserPreset: '',
-        plugins: [],
-        prompt: {},
+        ignores: [() => true],
+        parserPreset: {
+          name: '',
+          parserOpts: '',
+          path: '',
+        },
+        plugins: [
+          {
+            rules: {},
+          },
+          '',
+        ],
+        prompt: {
+          messages: {
+            emptyWarning: '',
+            lowerLimitWarning: '',
+            max: '',
+            min: '',
+            skip: '',
+            upperLimitWarning: '',
+          },
+          settings: {
+            enableMultipleScopes: true,
+            scopeEnumSeparator: '',
+          },
+          questions: {
+            header: {
+              description: '',
+              messages: {
+                customMessage: '',
+              },
+              enum: {
+                customProperty: {
+                  description: '',
+                  title: '',
+                  emoji: '',
+                },
+              },
+            },
+            type: {
+              description: '',
+              messages: {
+                customMessage: '',
+              },
+              enum: {
+                customProperty: {
+                  description: '',
+                  title: '',
+                  emoji: '',
+                },
+              },
+            },
+            scope: {
+              description: '',
+              messages: {
+                customMessage: '',
+              },
+              enum: {
+                customProperty: {
+                  description: '',
+                  title: '',
+                  emoji: '',
+                },
+              },
+            },
+            subject: {
+              description: '',
+              messages: {
+                customMessage: '',
+              },
+              enum: {
+                customProperty: {
+                  description: '',
+                  title: '',
+                  emoji: '',
+                },
+              },
+            },
+            body: {
+              description: '',
+              messages: {
+                customMessage: '',
+              },
+              enum: {
+                customProperty: {
+                  description: '',
+                  title: '',
+                  emoji: '',
+                },
+              },
+            },
+            footer: {
+              description: '',
+              messages: {
+                customMessage: '',
+              },
+              enum: {
+                customProperty: {
+                  description: '',
+                  title: '',
+                  emoji: '',
+                },
+              },
+            },
+            isBreaking: {
+              description: '',
+              messages: {
+                customMessage: '',
+              },
+              enum: {
+                customProperty: {
+                  description: '',
+                  title: '',
+                  emoji: '',
+                },
+              },
+            },
+            breakingBody: {
+              description: '',
+              messages: {
+                customMessage: '',
+              },
+              enum: {
+                customProperty: {
+                  description: '',
+                  title: '',
+                  emoji: '',
+                },
+              },
+            },
+            breaking: {
+              description: '',
+              messages: {
+                customMessage: '',
+              },
+              enum: {
+                customProperty: {
+                  description: '',
+                  title: '',
+                  emoji: '',
+                },
+              },
+            },
+            isIssueAffected: {
+              description: '',
+              messages: {
+                customMessage: '',
+              },
+              enum: {
+                customProperty: {
+                  description: '',
+                  title: '',
+                  emoji: '',
+                },
+              },
+            },
+            issuesBody: {
+              description: '',
+              messages: {
+                customMessage: '',
+              },
+              enum: {
+                customProperty: {
+                  description: '',
+                  title: '',
+                  emoji: '',
+                },
+              },
+            },
+            issues: {
+              description: '',
+              messages: {
+                customMessage: '',
+              },
+              enum: {
+                customProperty: {
+                  description: '',
+                  title: '',
+                  emoji: '',
+                },
+              },
+            },
+          },
+        },
         rules: {},
       }),
     ).toEqualTypeOf<UserConfig>();
